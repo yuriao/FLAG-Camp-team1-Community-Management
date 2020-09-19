b852735bf5cd15a6ec8abcb7b75a086d5794e482
tongsun27 <54683778+tongsun27@users.noreply.github.com>
2020-09-19 16:13:34 -0500

    login

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import TicketingManager from './pages/TicketingManager'
 import TicketingResident from './pages/TicketingResident'
+import RegistrationLogin from './pages/RegistrationLogIn.js';
+import RegistrationForm from './components/RegistrationForm.js';
 
 function App() {
   return (
@@ -33,6 +35,14 @@ function App() {
       <Route exact path='/TicketingManager' render={() =>
         <TicketingManager/>
       }/>
+      <Route exact path='/login' render={() =>
+        <RegistrationLogin/>
+      }/>
+
+    <Route exact path='/register' render={() =>
+        <RegistrationForm/>
+      }/>
+
     </Router>
   );
 }
