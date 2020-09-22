caf92b87379ce22ee72abf21fd27042a0405bce8
Tzuchieh Chen <tzuchieh@MacBook-Pro.local>
2020-09-22 12:34:57 -0400

    use java1.8, add User, UserType, Authorities

---
+++ communitymanagement/src/main/java/communitymanagement/model/User.java
@@ -0,0 +1,72 @@
+package communitymanagement.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Setter
+@Getter
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -2454867938054036364L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "user_type")
+	private UserType userType;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	// user name is the email address
+	@Column(name = "username")
+	private String userName;
+
+	private String password;
+
+	@Column(name = "phone_number")
+	private String phoneNumber;
+
+	private boolean enabled;
+
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Manager manager;
+
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Resident resident;
+
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Staff staff;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<Ticket> ticket;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<TicketWorkAssignee> ticketWorkAssignee;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<TicketComments> ticketComments;
+}
