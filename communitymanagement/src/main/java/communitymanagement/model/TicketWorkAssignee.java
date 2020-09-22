bbc210df617b93f2d1a48d8f090215d23f81cde7
tcchenbtx <tcchenbtx@gmail.com>
2020-09-21 21:06:26 -0400

    fix setting add models (#9)
    * fix backend setting + add models
    * snake case
    * fix format

---
+++ communitymanagement/src/main/java/communitymanagement/model/TicketWorkAssignee.java
@@ -0,0 +1,36 @@
+package communitymanagement.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.JoinColumn;
+
+@Entity
+@Table(name = "ticket_work_assignee")
+@Setter
+@Getter
+public class TicketWorkAssignee implements Serializable {
+
+	private static final long serialVersionUID = -2455867938054036364L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne
+	@JoinColumn(name = "ticket_id")
+	private Ticket ticket;
+}
