c31bbe7ad520f97fe411c40cde6c5466ce14e1e9
SherlockVWayne <48518962+SherlockVWayne@users.noreply.github.com>
2020-09-22 18:02:50 -0700

    Update Issue.java

--- communitymanagement/src/main/java/communitymanagement/model/Issue.java
+++ communitymanagement/src/main/java/communitymanagement/model/Issue.java
@@ -1,8 +1,6 @@
 package communitymanagement.model;
 
-
 import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,10 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.JoinColumn;
 
 @Entity
