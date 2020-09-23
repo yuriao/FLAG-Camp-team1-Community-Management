11750facd8162b212af336c2bee2225b386dd257
SherlockVWayne <48518962+SherlockVWayne@users.noreply.github.com>
2020-09-22 18:13:08 -0700

    Try1 (#12)
    * Add Location and Issue table
    * Model of Location and Issue
    * Update Issue.java
    * Update Issue.java
    * Update Location.java

---
+++ communitymanagement/src/main/java/communitymanagement/model/Issue.java
@@ -0,0 +1,30 @@
+package communitymanagement.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.JoinColumn;
+
+@Entity
+@Table(name = "issue")
+@Setter
+@Getter
+public class Issue implements Serializable {
+	
+	private static final long serialVersionUID = -2455867938054036364L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(name = "issue_type")
+	private String issueType;
+
+}
---
+++ communitymanagement/src/main/java/communitymanagement/model/Location.java
@@ -0,0 +1,29 @@
+package communitymanagement.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "location")
+@Setter
+@Getter
+public class Location implements Serializable {
+
+	private static final long serialVersionUID = -2455867938054036364L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@Column(name = "location_type")
+	private String locationType;
+}
