08e4be4478476ec4d4c19c256d4cc6d3154c0ea3
Tzuchieh Chen <tzuchieh@MacBook-Pro.local>
2020-09-24 21:17:27 -0400

    TicketWorkAssignee Dao+Service

---
+++ communitymanagement/src/main/java/communitymanagement/controller/TicketWorkAssigneeService.java
@@ -0,0 +1,34 @@
+package communitymanagement.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import communitymanagement.dao.TicketWorkAssigneeDao;
+import communitymanagement.model.TicketWorkAssignee;
+
+@Service
+public class TicketWorkAssigneeService {
+
+	@Autowired
+	private TicketWorkAssigneeDao ticketWorkAssigneeDao;
+
+	public void addTickeWorkAssignee(TicketWorkAssignee ticketWorkAssignee) {
+		ticketWorkAssigneeDao.addTickeWorkAssignee(ticketWorkAssignee);
+
+	}
+
+	public void deleteTickeWorkAssigneeById(int ticketWorkAssigneeId) {
+		ticketWorkAssigneeDao.deleteTickeWorkAssigneeById(ticketWorkAssigneeId);
+	}
+
+	public List<TicketWorkAssignee> getAllTicketWorkAssineeByTicketId(int ticketId) {
+		return ticketWorkAssigneeDao.getAllTicketWorkAssineeByTicketId(ticketId);
+	}
+
+	public List<TicketWorkAssignee> getAllTicketWorkAssineeByUserId(int userId) {
+		return ticketWorkAssigneeDao.getAllTicketWorkAssineeByUserId(userId);
+	}
+
+}
