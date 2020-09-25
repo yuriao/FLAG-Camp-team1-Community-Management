40e103693d63ea5040f29edbf62b2cc48511b01e
RainieHu <hxyyzptx@126.com>
2020-09-25 12:36:51 -0700

    add UserDao and UserService

---
+++ communitymanagement/src/main/java/communitymanagement/service/UserService.java
@@ -0,0 +1,26 @@
+package communitymanagement.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import communitymanagement.dao.UserDao;
+import communitymanagement.model.Resident;
+import communitymanagement.model.User;
+
+
+
+@Service
+public class UserService {
+	
+    @Autowired
+    private UserDao userDao;
+    
+    public void addUser(User user) {
+    	userDao.addUser(user);
+    }
+    public void getUserByUserName(String userName) {
+    	userDao.getUserByUserName(userName);
+    }
+    
+
+}
