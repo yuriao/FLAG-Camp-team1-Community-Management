4495bc42fac57a48d4ca78c029c312e0b635b084
ong Y <yxw1622@case.edu>
2020-09-28 01:04:26 -0400

    TicketDetailController

--- communitymanagement/src/main/java/communitymanagement/controller/TicketDetailController.java
+++ communitymanagement/src/main/java/communitymanagement/controller/TicketDetailController.java
@@ -1,5 +1,7 @@
 package communitymanagement.controller;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import communitymanagement.entity.SimpleComment;
 import communitymanagement.entity.SimpleResponse;
 import communitymanagement.entity.TicketCommentForm;
 import communitymanagement.entity.TicketForm;
@@ -16,6 +19,7 @@ import communitymanagement.model.Ticket;
 import communitymanagement.model.TicketComment;
 import communitymanagement.service.TicketCommentService;
 import communitymanagement.service.TicketService;
+import communitymanagement.service.UserService;
 
 @RestController
 public class TicketDetailController {
@@ -24,6 +28,9 @@ public class TicketDetailController {
 	
 	@Autowired
 	private TicketService ticketService;
+	
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping("/tickets/{ticket_id}/{user_id}")
 	public TicketForm getTicketDetail(@PathVariable("ticket_id") int ticketId, @PathVariable("user_id") int userId ) {
@@ -36,9 +43,19 @@ public class TicketDetailController {
 		return ticketForm;
 	}
 	
-	@PutMapping("/tickets/{ticket_id}/update")
-	public SimpleResponse update(@RequestBody TicketComment ticketComment, @PathVariable("ticket_id") int ticketId) {
+	@PutMapping("/tickets/{ticket_id}/update/{user_id}")
+	public SimpleResponse update(@RequestBody SimpleComment comment, @PathVariable("ticket_id") int ticketId, @PathVariable("user_id") int userId) {
 		//awaiting authentication
+		TicketComment ticketComment = new TicketComment();
+		ticketComment.setBody(comment.getComment());
+		ticketComment.setTicket(ticketService.getTicketById(ticketId));
+		ticketComment.setUser(userService.getUserByUserId(userId));
+		
+		Date date = new Date();
+		Timestamp curTime = new Timestamp(date.getTime());
+		ticketComment.setCreated(curTime);;
+		ticketCommentService.addTicketComment(ticketComment);
+		
 		String msg = "Comment success";
 		SimpleResponse simpleResponse = SimpleResponse.builder().status("200").message(msg).build();
 		return simpleResponse;
