1db7a31a9fa33a2432630fb8b214ed89a09ed4c5
yongyw <71576032+yongyw@users.noreply.github.com>
2020-09-28 19:49:47 -0400

    TicketCommentController and TicketCommentDao (#38)
    * TicketDetailController and TicketCommentDao
    * TicketDetailController
    * TicketCommentController

--- communitymanagement/src/main/java/communitymanagement/controller/TicketCommentController.java
+++ communitymanagement/src/main/java/communitymanagement/controller/TicketCommentController.java
@@ -1,17 +1,22 @@
 package communitymanagement.controller;
 
 import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import communitymanagement.entity.SimpleComment;
 import communitymanagement.entity.TicketCommentForm;
+import communitymanagement.entity.TicketForm;
 import communitymanagement.model.Ticket;
 import communitymanagement.model.TicketComment;
 import communitymanagement.model.User;
@@ -103,5 +108,36 @@ public class TicketCommentController {
 		}
 		return ResponseEntity.status(HttpStatus.OK).body(msg);
 	}
-
+	
+	@GetMapping("/tickets/{ticket_id}/{user_id}")
+	public ResponseEntity<TicketForm> getTicketDetail(@PathVariable("ticket_id") int ticketId, @PathVariable("user_id") int userId) {
+		//awaiting authentication
+		TicketForm ticketForm = new TicketForm();
+		Ticket ticket = ticketService.getTicketById(ticketId);
+		List<TicketComment> ticketComment = ticketCommentService.getAllTicketComments(ticketId);
+		ticketForm.setTicket(ticket);
+		ticketForm.setTicketComment(ticketComment);
+		return ResponseEntity.status(HttpStatus.OK).body(ticketForm);
+	}
+	
+	@PutMapping("/tickets/{ticket_id}/update/{user_id}")
+	public ResponseEntity<String> update(@RequestBody SimpleComment comment, @PathVariable("ticket_id") int ticketId, @PathVariable("user_id") int userId, BindingResult result) {
+		//awaiting authentication
+		if (result.hasErrors()) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body("Fail to build the ticket comment form");
+		}
+		TicketComment ticketComment = new TicketComment();
+		ticketComment.setBody(comment.getComment());
+		ticketComment.setTicket(ticketService.getTicketById(ticketId));
+		ticketComment.setUser(userService.getUserByUserId(userId));
+		
+		Date date = new Date();
+		Timestamp curTime = new Timestamp(date.getTime());
+		ticketComment.setCreated(curTime);;
+		ticketCommentService.addTicketComment(ticketComment);
+		
+		String msg = "Comment Added.";
+		return ResponseEntity.status(HttpStatus.OK).body(msg);
+	}
 }
---
+++ communitymanagement/src/main/java/communitymanagement/entity/SimpleComment.java
@@ -0,0 +1,8 @@
+package communitymanagement.entity;
+
+import lombok.Data;
+
+@Data
+public class SimpleComment {
+	private String comment;
+}
