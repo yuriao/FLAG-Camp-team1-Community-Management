563265b84bce5936ab7ea14b1eac82424b7680f2
Tzuchieh Chen <tzuchieh@MacBook-Pro.local>
2020-10-03 00:22:56 -0400

    tune spring security login

--- communitymanagement/src/main/java/communitymanagement/controller/HomePageController.java
+++ communitymanagement/src/main/java/communitymanagement/controller/HomePageController.java
@@ -1,8 +1,12 @@
 package communitymanagement.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -10,22 +14,34 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import communitymanagement.model.User;
+import communitymanagement.service.UserService;
 
 @RestController
 public class HomePageController {
+	
+	@Autowired
+	private UserService userService;
 
 	@PostMapping("/login")
-	public ResponseEntity<String> login(@RequestParam(value = "error", required = false) String error,
-			@RequestParam(value = "logout", required = false) String logout) {
-		if (error != null) {
-			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid username and Password");
+	public ResponseEntity<Map<String, String>> login(@RequestBody User user) {
+		System.out.println(user.getPassword());
+		System.out.println(user.getUsername());
+		
+		User attemptUser = null;
+		attemptUser = userService.getUserByUsername(user.getUsername());
+		Map<String, String> result = new HashMap<>();
+		if (attemptUser != null && user.getPassword().equals(attemptUser.getPassword())) {
+			result.put("firstName", attemptUser.getFirstName());
+			result.put("lastName", attemptUser.getLastName());
+			result.put("message", "Logged in successfully");
+			return ResponseEntity.status(HttpStatus.OK).body(result);
 		}
-		if (logout != null) {
-			return ResponseEntity.status(HttpStatus.OK).body("You have logged out successfully");
-		}
-		return ResponseEntity.status(HttpStatus.OK).body("You have logged in successfully");
+		result.put("message", "Wrong username or password");
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(result);
 	}
 	
 	@GetMapping(value="/logout")
