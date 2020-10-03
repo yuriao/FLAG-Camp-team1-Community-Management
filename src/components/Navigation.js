63ec1bba49d9245b6e9811d64f92b69499657a35
Dongdong Zhang <dongdongzhang@Dongdongs-MacBook-Pro.local>
2020-10-02 21:36:16 -0700

    nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,10 +4,10 @@ class Navigation extends Component {
     render() {
         return (
             <div className = "nav">
-                <a href = "#dashboard" className = "left-nav">Dashboard</a>
-                <a href = "#ticket" className = "left-nav">Ticket</a>
-                <a href = "#chat" className = "left-nav">Chat</a>
-                <a href = "#logout" className = "right-nav">Logout</a>
+                <a href = {this.props.dashboard} className = "left-nav">Dashboard</a>
+                <a href = {this.props.ticket} className = "left-nav">Ticket</a>
+                <a href = {this.props.chat} className = "left-nav">Chat</a>
+                <a href = {this.props.logout} className = "right-nav">Logout</a>
             </div>
         );
     }
