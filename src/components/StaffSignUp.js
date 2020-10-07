931792682cf30282aa4f6366e66cb839e9aefbd5
Tzuchieh Chen <tzuchieh@MacBook-Pro.local>
2020-10-07 00:21:14 -0400

    change the hard code id

--- src/components/StaffSignUp.js
+++ src/components/StaffSignUp.js
@@ -40,15 +40,13 @@ class StaffSignUp extends Component {
                         style={{ width: '100%' }}
                         onChange={this.handleChange}
                     >
-                        <Option value="147">Appliances Repair</Option>
-                        <Option value="1">Appliances</Option>
-                        <Option value="2">Cable</Option>
-                        <Option value="3">Water</Option>
-                        <Option value="4">Electricity</Option>
-                        <Option value="5">Floor</Option>
-                        <Option value="6">Door</Option>
-                        <Option value="144">Plumbing</Option>
-                        <Option value="149">Handyman</Option>
+                        <Option value="152">Appliances Repair</Option>
+                        <Option value="167">Cable</Option>
+                        <Option value="165">Water</Option>
+                        <Option value="154">Electricity</Option>
+                        <Option value="163">Floor</Option>
+                        <Option value="147">Plumbing</Option>
+                        <Option value="157">Handyman</Option>
                     </Select>
                     </Form.Item>
 
